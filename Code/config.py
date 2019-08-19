cb429d0b179e29e3967d59a03878d0c74339827b
Kenny Cutler <48301084+k-cutler@users.noreply.github.com>
2019-08-19 14:33:13 -0500

    Update config.py

--- Code/config.py
+++ Code/config.py
@@ -2,9 +2,3 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 gkey = 'YOUR API KEY HERE'
-=======
-gkey = 'AIzaSyC9TDMqPtO4HXBj3sVjyjYZKf54GJbonUk'
->>>>>>> kenny
-=======
-gkey = 'YOUR API KEY HERE'
->>>>>>> 0d74c9ddc39b0371269a3c475d9a381a3129e566
