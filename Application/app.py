bc03777fcc9d149a236ba820884142f4603eac9f
mckc661 <mcoonskc@yahoo.com>
2019-08-23 18:48:10 -0500

    new app.py

--- Application/app.py
+++ Application/app.py
@@ -21,144 +21,96 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db4.sqlite"
 db = SQLAlchemy(app)
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
+db.Model.metadata.reflect(db.engine)
 
-# Save references to each table
-Chinese_Table = Base.classes.chinese
-# mexican = Base.classes.mexican
-# gyms = Base.classes.gyms
-# gas_stations = Base.classes.gas_stations
-# schools = Base.classes.schools
+class Chinese_Table(db.Model):
+    __tablename__ = 'chinese'
+    __table_args__={'extend_existing':True}
+    id = db.Column(db.Text, primary_key=True)
+
+class Mexican_Table(db.Model):
+    __tablename__ = 'mexican'
+    __table_args__={'extend_existing':True}
+    id = db.Column(db.Text, primary_key=True)
+
+
+# Create database tables
+@app.before_first_request
+def setup():
+    # Recreate database each time for demo
+    # db.drop_all()
+    db.create_all()
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(db.engine, reflect=True)
+
+# # Save references to each table
+# Chinese_Table = Base.classes.chinese
+# # mexican = Base.classes.mexican
+# # gyms = Base.classes.gyms
+# # gas_stations = Base.classes.gas_stations
+# # schools = Base.classes.schools
 
 @app.route("/")
 def index():
     """Return the homepage."""
-    return render_template("index.html")
+
+    Place1=Chinese_Table.query.filter_by(City='Overland Park').distinct()
+    return render_template("index.html", counts=Place1)
 
 
-@app.route("/chinese/lat")
-def chinese_lat():
-    """Return a list of sample names."""
+# @app.route("/test")
+# def test1():
+#     """Return the homepage."""
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Chinese_Table).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+#     Place2=Mexican_Table.query.filter_by(City='Overland Park').distinct()
+#     return render_template("test.html", locations=Place2)
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df["Place Latitude"]))
 
-@app.route("/chinese/lon")
-def chinese_lon():
-    """Return a list of sample names."""
+@app.route("/test2")
+def test2():
 
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(Chinese_Table).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    results = db.session.query(Chinese_Table.PlaceLatitude, Chinese_Table.PlaceLongitude ).\
+        filter_by(City='Overland Park').distinct()
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df["Place Longitude"]))
-    
+    # Create lists from the query results
+    latitude_mx = [result[0] for result in results]
+    longitude_mx = [result[1] for result in results]
 
-# initial chart showing the highest rated city by category
-
-# @app.route("/mexican")
-# def mexican():
+    # Generate the plot trace
+    trace = {
+        "lat": latitude_mx,
+        "lng": longitude_mx
    
-# #     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Mexican).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
+    }
+    #  return render_template("test.html", locations=trace)
+    return jsonify(trace)
 
-# #     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
+@app.route("/test3")
+def test3():
 
-# @app.route("/gyms")
-# def gyms():
+    results = db.session.query(Chinese_Table.PlaceRating, Chinese_Table.PlaceName, Chinese_Table.PlaceLatitude, Chinese_Table.PlaceLongitude ).\
+        filter_by(City='Overland Park').distinct()
 
+    # Create lists from the query results
+    rating1 = [result[0] for result in results]
+    name1 = [result[1] for result in results]
+    latitude_mx = [result[2] for result in results]
+    longitude_mx = [result[3] for result in results]
 
-# #     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Gyms).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
+    # Generate the plot trace
+    trace2 = {
+        "name" : name1,
+        "lat": latitude_mx,
+        "lng": longitude_mx,
+        "rating": rating1
+   
+    }
+    #  return render_template("test.html", locations=trace2)
+    return jsonify(trace2)
 
-#     # Return a list of the column names (sample names)
-# #     return jsonify(list(df.columns)[2:])
-
-# @app.route("/gas_stations")
-# def gas_stations():
-
-
-# #     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Gas_stations).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
-
-# @app.route("/schools")
-# def schools():
-# #     """Return a list of sample names."""
-
-# #     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Schools).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-# #     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
-
-
-# # @app.route("/metadata/<sample>")
-# # def sample_metadata(sample):
-# #     """Return the MetaData for a given sample."""
-# #     sel = [
-# #         Samples_Metadata.City,
-# #         Samples_Metadata.Lat,
-# #         Samples_Metadata.Long,
-# #         Samples_Metadata.Name,
-# #         Samples_Metadata.Address,
-# #         Samples_Metadata.Rating,
-        
-# #     ]
-
-# #     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
-
-# #     # Create a dictionary entry for each row of metadata information
-# #     sample_metadata = {}
-# #     for result in results:
-# #         sample_metadata["sample"] = result[0]
-# #         sample_metadata["ETHNICITY"] = result[1]
-# #         sample_metadata["GENDER"] = result[2]
-# #         sample_metadata["AGE"] = result[3]
-# #         sample_metadata["LOCATION"] = result[4]
-# #         sample_metadata["BBTYPE"] = result[5]
-# #         sample_metadata["WFREQ"] = result[6]
-
-# #     print(sample_metadata)
-# #     return jsonify(sample_metadata)
-
-
-# # @app.route("/samples/<sample>")
-# # def samples(sample):
-# #     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-# #     stmt = db.session.query(Samples).statement
-# #     df = pd.read_sql_query(stmt, db.session.bind)
-
-# #     # Filter the data based on the sample number and
-# #     # only keep rows with values above 1
-# #     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-
-# #     # Sort by sample
-# #     sample_data.sort_values(by=sample, ascending=False, inplace=True)
-
-# #     # Format the data to send as json
-# #     data = {
-# #         "otu_ids": sample_data.otu_id.values.tolist(),
-# #         "sample_values": sample_data[sample].values.tolist(),
-# #         "otu_labels": sample_data.otu_label.tolist(),
-# #     }
-# #     return jsonify(data)
-
-
+    
 if __name__ == "__main__":
     app.run()
