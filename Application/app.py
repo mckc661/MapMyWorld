a65e93b4655b1e2ec68781da5b49f6ddad90f2c3
Kenneth Cutler <kennethcutler@Kenneths-MacBook-Pro.local>
2019-08-24 08:14:48 -0500

    correct flask, js, index

--- Application/app.py
+++ Application/app.py
@@ -18,19 +18,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-<<<<<<< HEAD
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db4.sqlite"
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db3.sqlite"
-=======
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db5.sqlite"
->>>>>>> Stashed changes
-=======
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db5.sqlite"
->>>>>>> Stashed changes
->>>>>>> kenny
 db = SQLAlchemy(app)
 
 db.Model.metadata.reflect(db.engine)
@@ -40,16 +28,10 @@ class Chinese_Table(db.Model):
     __table_args__={'extend_existing':True}
     id = db.Column(db.Text, primary_key=True)
 
-<<<<<<< HEAD
 class Mexican_Table(db.Model):
     __tablename__ = 'mexican'
     __table_args__={'extend_existing':True}
     id = db.Column(db.Text, primary_key=True)
-=======
-@app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
 
 class Gas_Stations_Table(db.Model):
     __tablename__ = 'gas_stations'
@@ -61,53 +43,10 @@ class Gyms_Table(db.Model):
     __table_args__={'extend_existing':True}
     id = db.Column(db.Text, primary_key=True)
 
-<<<<<<< Updated upstream
-=======
-class Gas_Stations_Table(db.Model):
-    __tablename__ = 'gas_stations'
-    __table_args__={'extend_existing':True}
-    id = db.Column(db.Text, primary_key=True)
-
-class Gyms_Table(db.Model):
-    __tablename__ = 'gyms'
-    __table_args__={'extend_existing':True}
-    id = db.Column(db.Text, primary_key=True)
-
->>>>>>> Stashed changes
 class Schools_Table(db.Model):
     __tablename__ = 'schools'
     __table_args__={'extend_existing':True}
     id = db.Column(db.Text, primary_key=True)
-
-@app.route("/chinese")
-def chinese():
-    """Return a list of sample names."""
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    # Use Pandas to perform the sql query
-    stmt = db.session.query(chinese).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
-
-# initial chart showing the highest rated city by category
-
-# @app.route("/mexican")
-# def mexican():
-   
-#     # Use Pandas to perform the sql query
-#     stmt = db.session.query(Mexican).statement
-#     df = pd.read_sql_query(stmt, db.session.bind)
-
-#     # Return a list of the column names (sample names)
-#     return jsonify(list(df.columns)[2:])
-
-# @app.route("/gyms")
-# def gyms():
->>>>>>> kenny
-
 
 # Create database tables
 @app.before_first_request
@@ -116,80 +55,6 @@ def setup():
     # db.drop_all()
     db.create_all()
 
-# # reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(db.engine, reflect=True)
-
-# # Save references to each table
-# Chinese_Table = Base.classes.chinese
-# # mexican = Base.classes.mexican
-# # gyms = Base.classes.gyms
-# # gas_stations = Base.classes.gas_stations
-# # schools = Base.classes.schools
-
-@app.route("/")
-def index():
-    """Return the homepage."""
-
-    Place1=Chinese_Table.query.filter_by(City='Overland Park').distinct()
-    return render_template("index.html", counts=Place1)
-
-
-# @app.route("/test")
-# def test1():
-#     """Return the homepage."""
-
-#     Place2=Mexican_Table.query.filter_by(City='Overland Park').distinct()
-#     return render_template("test.html", locations=Place2)
-
-
-@app.route("/test2")
-def test2():
-
-    results = db.session.query(Chinese_Table.PlaceLatitude, Chinese_Table.PlaceLongitude ).\
-        filter_by(City='Overland Park').distinct()
-
-    # Create lists from the query results
-    latitude_mx = [result[0] for result in results]
-    longitude_mx = [result[1] for result in results]
-
-    # Generate the plot trace
-    trace = {
-        "lat": latitude_mx,
-        "lng": longitude_mx
-   
-    }
-    #  return render_template("test.html", locations=trace)
-    return jsonify(trace)
-
-@app.route("/test3")
-def test3():
-
-    results = db.session.query(Chinese_Table.PlaceRating, Chinese_Table.PlaceName, Chinese_Table.PlaceLatitude, Chinese_Table.PlaceLongitude ).\
-        filter_by(City='Overland Park').distinct()
-
-    # Create lists from the query results
-    rating1 = [result[0] for result in results]
-    name1 = [result[1] for result in results]
-    latitude_mx = [result[2] for result in results]
-    longitude_mx = [result[3] for result in results]
-
-    # Generate the plot trace
-    trace2 = {
-        "name" : name1,
-        "lat": latitude_mx,
-        "lng": longitude_mx,
-        "rating": rating1
-   
-    }
-    #  return render_template("test.html", locations=trace2)
-    return jsonify(trace2)
-
-<<<<<<< HEAD
-    
-=======
-=======
 @app.route("/")
 def index():
     """Return the homepage."""
@@ -277,95 +142,6 @@ def schools_results():
     results = db.session.query(Schools_Table.PlaceRating, Schools_Table.PlaceName).\
         filter_by(City='Overland Park').distinct()
 
-=======
-@app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
-
-@app.route("/chinese")
-def chinese_results():
-
-    results = db.session.query(Chinese_Table.PlaceRating, Chinese_Table.PlaceName).\
-        filter_by(City='Overland Park').distinct()
-
-    # Create lists from the query results
-    rating = [result[0] for result in results]
-    name = [result[1] for result in results]
-
-    # Generate the plot trace
-    trace = {
-        "x" : name,
-        "y": rating,
-        "type": "bar"
-    }
-    #  return render_template("test.html", locations=trace2)
-    return jsonify(trace)
-
-@app.route("/mexican")
-def mexican_results():
-
-    results = db.session.query(Mexican_Table.PlaceRating, Mexican_Table.PlaceName).\
-        filter_by(City='Overland Park').distinct()
-
-    # Create lists from the query results
-    rating2 = [result[0] for result in results]
-    name2 = [result[1] for result in results]
-
-    # Generate the plot trace
-    trace2 = {
-        "x" : name2,
-        "y": rating2,
-        "type": "bar"
-    }
-    #  return render_template("test.html", locations=trace)
-    return jsonify(trace2)
-
-@app.route("/gas_stations")
-def gas_stations_results():
-
-    results = db.session.query(Gas_Stations_Table.PlaceRating, Gas_Stations_Table.PlaceName).\
-        filter_by(City='Overland Park').distinct()
-
-    # Create lists from the query results
-    rating3 = [result[0] for result in results]
-    name3 = [result[1] for result in results]
-
-    # Generate the plot trace
-    trace3 = {
-        "x" : name3,
-        "y": rating3,
-        "type": "bar"
-    }
-    #  return render_template("test.html", locations=trace2)
-    return jsonify(trace3)
-
-@app.route("/gyms")
-def gyms_results():
-
-    results = db.session.query(Gyms_Table.PlaceRating, Gyms_Table.PlaceName).\
-        filter_by(City='Overland Park').distinct()
-
-    # Create lists from the query results
-    rating4 = [result[0] for result in results]
-    name4 = [result[1] for result in results]
-
-    # Generate the plot trace
-    trace4 = {
-        "x" : name4,
-        "y": rating4,
-        "type": "bar"
-    }
-    #  return render_template("test.html", locations=trace2)
-    return jsonify(trace4)
-
-@app.route("/schools")
-def schools_results():
-
-    results = db.session.query(Schools_Table.PlaceRating, Schools_Table.PlaceName).\
-        filter_by(City='Overland Park').distinct()
-
->>>>>>> Stashed changes
     # Create lists from the query results
     rating5 = [result[0] for result in results]
     name5 = [result[1] for result in results]
@@ -379,7 +155,5 @@ def schools_results():
     #  return render_template("test.html", locations=trace2)
     return jsonify(trace5)
     
->>>>>>> Stashed changes
->>>>>>> kenny
 if __name__ == "__main__":
     app.run()
--- Application/static/js/app.js
+++ Application/static/js/app.js
@@ -1,82 +1,3 @@
-<<<<<<< HEAD
-var responselat = d3.json("/chinese/lat");
-var responselon = d3.json("/chinese/lon");
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-// function buildMetadata(sample) {
->>>>>>> kenny
-
-console.log(responselat)
-console.log(responselon)
-
-
-
-
-
-function createMap(places) {
-
-  // Create the tile layer that will be the background of our map
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.light",
-    accessToken: API_KEY
-  });
-
-  // Create a baseMaps object to hold the lightmap layer
-  var baseMaps = {
-    "Light Map": lightmap
-  };
-
-  // Create an overlayMaps object to hold the bikeStations layer
-  var overlayMaps = {
-    "places": Places
-  };
-
-  // Create the map object with options
-  var map = L.map("map-id", {
-    center: [39.096977, -94.578681],
-    zoom: 12,
-    layers: [lightmap, places]
-  });
-
-  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(map);
-}
-
-function createMarkers(response) {
-
-  // Pull the "stations" property off of response.data
-  // var places = response.Place_Name;
-  // // Initialize an array to hold bike markers
-  // var placeMarkers = [];
-  // Object.entries(response).forEach(([key, value]) => 
-  //   console.log(`Key: ${key} and Value ${value}`)
-  
-  // Loop through the stations array
-  for (var index = 0; index < responselat.length; index++) {
-
-    // For each station, create a marker and bind a popup with the station's name
-    var placeMarker = L.marker(responselat, responselon)
-      .bindPopup("<h3>" + responselat + responselon + "<h3>");
-
-    // Add the marker to the bikeMarkers array
-    placeMarkers.push(placeMarker);
-  }
-
-
-  // // Create a layer group made from the bike markers array, pass it into the createMap function
-createMap(L.layerGroup(placeMarkers));
-};
-
-<<<<<<< HEAD
-=======
-// Initialize the dashboard
-init();
-=======
 var defaultURL = "/chinese";
 d3.json(defaultURL).then(function(data) {
     var data = [data];
@@ -90,31 +11,10 @@ function updatePlotly(newdata) {
     Plotly.restyle("bar", "y", [newdata.y]);
 }
 
-=======
-var defaultURL = "/chinese";
-d3.json(defaultURL).then(function(data) {
-    var data = [data];
-    var layout = { margin: { t: 30, b: 100 } };
-    Plotly.plot("bar", data, layout);
-});
-
-// Update the plot with new data
-function updatePlotly(newdata) {
-    Plotly.restyle("bar", "x", [newdata.x]);
-    Plotly.restyle("bar", "y", [newdata.y]);
-}
-
->>>>>>> Stashed changes
 function getData(route) {
     console.log(route);
     d3.json(`/${route}`).then(function(data){
       console.log("newdata", data);
       updatePlotly(data);
     });
-<<<<<<< Updated upstream
 }
->>>>>>> Stashed changes
-=======
-}
->>>>>>> Stashed changes
->>>>>>> kenny
