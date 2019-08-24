a33f607706b661f6ee6b9cd27d650d5af7e4d1a5
Kenneth Cutler <kennethcutler@Kenneths-MacBook-Pro.local>
2019-08-23 21:29:40 -0500

    updated flask, js, index

--- Application/app.py
+++ Application/app.py
@@ -18,7 +18,11 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
+<<<<<<< Updated upstream
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db3.sqlite"
+=======
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/places_db5.sqlite"
+>>>>>>> Stashed changes
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
@@ -38,11 +42,26 @@ def index():
     """Return the homepage."""
     return render_template("index.html")
 
+class Gas_Stations_Table(db.Model):
+    __tablename__ = 'gas_stations'
+    __table_args__={'extend_existing':True}
+    id = db.Column(db.Text, primary_key=True)
+
+class Gyms_Table(db.Model):
+    __tablename__ = 'gyms'
+    __table_args__={'extend_existing':True}
+    id = db.Column(db.Text, primary_key=True)
+
+class Schools_Table(db.Model):
+    __tablename__ = 'schools'
+    __table_args__={'extend_existing':True}
+    id = db.Column(db.Text, primary_key=True)
 
 @app.route("/chinese")
 def chinese():
     """Return a list of sample names."""
 
+<<<<<<< Updated upstream
     # Use Pandas to perform the sql query
     stmt = db.session.query(chinese).statement
     df = pd.read_sql_query(stmt, db.session.bind)
@@ -148,5 +167,107 @@ def chinese():
 #     return jsonify(data)
 
 
+=======
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+@app.route("/chinese")
+def chinese_results():
+
+    results = db.session.query(Chinese_Table.PlaceRating, Chinese_Table.PlaceName).\
+        filter_by(City='Overland Park').distinct()
+
+    # Create lists from the query results
+    rating = [result[0] for result in results]
+    name = [result[1] for result in results]
+
+    # Generate the plot trace
+    trace = {
+        "x" : name,
+        "y": rating,
+        "type": "bar"
+    }
+    #  return render_template("test.html", locations=trace2)
+    return jsonify(trace)
+
+@app.route("/mexican")
+def mexican_results():
+
+    results = db.session.query(Mexican_Table.PlaceRating, Mexican_Table.PlaceName).\
+        filter_by(City='Overland Park').distinct()
+
+    # Create lists from the query results
+    rating2 = [result[0] for result in results]
+    name2 = [result[1] for result in results]
+
+    # Generate the plot trace
+    trace2 = {
+        "x" : name2,
+        "y": rating2,
+        "type": "bar"
+    }
+    #  return render_template("test.html", locations=trace)
+    return jsonify(trace2)
+
+@app.route("/gas_stations")
+def gas_stations_results():
+
+    results = db.session.query(Gas_Stations_Table.PlaceRating, Gas_Stations_Table.PlaceName).\
+        filter_by(City='Overland Park').distinct()
+
+    # Create lists from the query results
+    rating3 = [result[0] for result in results]
+    name3 = [result[1] for result in results]
+
+    # Generate the plot trace
+    trace3 = {
+        "x" : name3,
+        "y": rating3,
+        "type": "bar"
+    }
+    #  return render_template("test.html", locations=trace2)
+    return jsonify(trace3)
+
+@app.route("/gyms")
+def gyms_results():
+
+    results = db.session.query(Gyms_Table.PlaceRating, Gyms_Table.PlaceName).\
+        filter_by(City='Overland Park').distinct()
+
+    # Create lists from the query results
+    rating4 = [result[0] for result in results]
+    name4 = [result[1] for result in results]
+
+    # Generate the plot trace
+    trace4 = {
+        "x" : name4,
+        "y": rating4,
+        "type": "bar"
+    }
+    #  return render_template("test.html", locations=trace2)
+    return jsonify(trace4)
+
+@app.route("/schools")
+def schools_results():
+
+    results = db.session.query(Schools_Table.PlaceRating, Schools_Table.PlaceName).\
+        filter_by(City='Overland Park').distinct()
+
+    # Create lists from the query results
+    rating5 = [result[0] for result in results]
+    name5 = [result[1] for result in results]
+
+    # Generate the plot trace
+    trace5 = {
+        "x": name5,
+        "y": rating5,
+        "type": "bar"
+    }
+    #  return render_template("test.html", locations=trace2)
+    return jsonify(trace5)
+    
+>>>>>>> Stashed changes
 if __name__ == "__main__":
     app.run()
--- Application/static/js/app.js
+++ Application/static/js/app.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
@@ -112,3 +113,25 @@ function optionChanged(newCategory) {
 
 // Initialize the dashboard
 init();
+=======
+var defaultURL = "/chinese";
+d3.json(defaultURL).then(function(data) {
+    var data = [data];
+    var layout = { margin: { t: 30, b: 100 } };
+    Plotly.plot("bar", data, layout);
+});
+
+// Update the plot with new data
+function updatePlotly(newdata) {
+    Plotly.restyle("bar", "x", [newdata.x]);
+    Plotly.restyle("bar", "y", [newdata.y]);
+}
+
+function getData(route) {
+    console.log(route);
+    d3.json(`/${route}`).then(function(data){
+      console.log("newdata", data);
+      updatePlotly(data);
+    });
+}
+>>>>>>> Stashed changes
